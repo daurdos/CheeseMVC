dc3d917a34f183e5133602ce80fbdf43920248c3
daurdos <daurdos@gmail.com>
2019-08-13 10:37:31 +0600

    simple many to many program net core

---
+++ CheeseMVC/Data/CheeseDbContext.cs
@@ -0,0 +1,33 @@
+﻿using CheeseMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Data
+{
+    public class CheeseDbContext : DbContext
+    {
+        public CheeseDbContext(DbContextOptions<CheeseDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CheeseMVC.Models.Cheese> Cheeses { get; set; }
+        public DbSet<CheeseMVC.Models.CheeseCategory> Categories { get; set; }
+        public DbSet<CheeseMVC.Models.Menu> Menus { get; set; }
+        public DbSet<CheeseMVC.Models.CheeseMenu> CheeseMenus { get; set; }
+
+
+
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CheeseMenu>()
+                .HasKey(c => new { c.CheeseID, c.MenuID });
+        }
+        
+          
+    }
+}
---
+++ CheeseMVC/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CheeseDbContext>
+    {
+        public CheeseDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<CheeseDbContext>();
+
+            var connectionString = configuration.GetConnectionString("CheeseDbContext");
+
+            builder.UseSqlServer(connectionString);
+
+            return new CheeseDbContext(builder.Options);
+        }
+    }
+}
